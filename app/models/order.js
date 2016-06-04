20becee00fda6972cd474e49a7553c0786962540
Lucas Nelson <lucas-nelson@users.noreply.github.com>
2016-06-04 22:12:09 +1000

    Add some validations to the model

--- app/models/order.js
+++ app/models/order.js
@@ -1,12 +1,23 @@
+import EmberValidations from 'ember-validations';
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 
-export default Model.extend({
+export default Model.extend(EmberValidations, {
   account: attr('string'),
   direction: attr('string'),
   orderType: attr('string'),
   price: attr('dollars'),
   quantity: attr('number'),
   symbol: attr('string'),
-  venue: attr('string')
+  venue: attr('string'),
+
+  validations: {
+    account: { presence: true },
+    direction: { format: { with: /^(buy|sell)$/ } },
+    orderType: { format: { with: /^(fill-or-kill|immediate-or-cancel|limit|market)$/ } },
+    price: { numericality: { greaterThan: 0, onlyInteger: true } },
+    quantity: { numericality: { greaterThan: 0, onlyInteger: true } },
+    symbol: { presence: true },
+    venue: { presence: true }
+  }
 });
--- tests/unit/models/order-test.js
+++ tests/unit/models/order-test.js
@@ -1,8 +1,16 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
+import {
+  testValidPropertyValues,
+  testInvalidPropertyValues
+} from '../../helpers/validate-properties';
 
 moduleForModel('order', 'Unit | Model | order', {
   // Specify the other units that are required for this test.
-  needs: ['transform:dollars']
+  needs: ['ember-validations@validator:local/format',
+          'ember-validations@validator:local/numericality',
+          'ember-validations@validator:local/presence',
+          'transform:dollars']
 });
 
 test('it exists', function(assert) {
@@ -13,42 +21,83 @@ test('it exists', function(assert) {
 
 test('it has attribute: account', function(assert) {
   const model = this.subject();
-  var hasAttr = Object.keys(model.toJSON()).indexOf('account') > -1;
+  const hasAttr = Object.keys(model.toJSON()).indexOf('account') > -1;
   assert.ok(hasAttr);
 });
 
 test('it has attribute: direction', function(assert) {
   const model = this.subject();
-  var hasAttr = Object.keys(model.toJSON()).indexOf('direction') > -1;
+  const hasAttr = Object.keys(model.toJSON()).indexOf('direction') > -1;
   assert.ok(hasAttr);
 });
 
 test('it has attribute: order type', function(assert) {
   const model = this.subject();
-  var hasAttr = Object.keys(model.toJSON()).indexOf('orderType') > -1;
+  const hasAttr = Object.keys(model.toJSON()).indexOf('orderType') > -1;
   assert.ok(hasAttr);
 });
 
 test('it has attribute: price', function(assert) {
   const model = this.subject();
-  var hasAttr = Object.keys(model.toJSON()).indexOf('price') > -1;
+  const hasAttr = Object.keys(model.toJSON()).indexOf('price') > -1;
   assert.ok(hasAttr);
 });
 
 test('it has attribute: quantity', function(assert) {
   const model = this.subject();
-  var hasAttr = Object.keys(model.toJSON()).indexOf('quantity') > -1;
+  const hasAttr = Object.keys(model.toJSON()).indexOf('quantity') > -1;
   assert.ok(hasAttr);
 });
 
 test('it has attribute: symbol', function(assert) {
   const model = this.subject();
-  var hasAttr = Object.keys(model.toJSON()).indexOf('symbol') > -1;
+  const hasAttr = Object.keys(model.toJSON()).indexOf('symbol') > -1;
   assert.ok(hasAttr);
 });
 
 test('it has attribute: venue', function(assert) {
   const model = this.subject();
-  var hasAttr = Object.keys(model.toJSON()).indexOf('venue') > -1;
+  const hasAttr = Object.keys(model.toJSON()).indexOf('venue') > -1;
   assert.ok(hasAttr);
+});
+
+testValidPropertyValues('account', ['foo']);
+testInvalidPropertyValues('account', ['']);
+
+testValidPropertyValues('direction', ['buy', 'sell']);
+testInvalidPropertyValues('direction', ['', 'foo']);
+
+testValidPropertyValues('orderType', ['fill-or-kill', 'immediate-or-cancel', 'limit', 'market']);
+testInvalidPropertyValues('orderType', ['', 'foo']);
+
+testValidPropertyValues('price', [1, 150]);
+testInvalidPropertyValues('price', [0, 2.5]);
+
+testValidPropertyValues('quantity', [1]);
+testInvalidPropertyValues('quantity', [0, 1.5]);
+
+testValidPropertyValues('symbol', ['foo']);
+testInvalidPropertyValues('symbol', ['']);
+
+testValidPropertyValues('venue', ['foo']);
+testInvalidPropertyValues('venue', ['']);
+
+
+test('it should be invalid', function(assert) {
+  const model = this.subject();
+  assert.equal(model.get('isValid'), false, 'order is valid');
+});
+
+test('it should be valid', function(assert) {
+  const model = this.subject();
+  Ember.run(function() {
+    model.set('account', 'foo');
+    model.set('direction', 'buy');
+    model.set('orderType', 'limit');
+    model.set('price', '25000');
+    model.set('quantity', '100');
+    model.set('symbol', 'foo');
+    model.set('venue', 'foo');
+  });
+  assert.equal(model.get('isValid'), true, 'order is invalid');
 });
